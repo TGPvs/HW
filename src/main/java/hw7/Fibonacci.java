599c837f0daf2194921b7bfb0b84ba99eab84424
TGPvs <tgpavs@gmail.com>
2024-07-10 14:15:16 +0300

    next hw update

--- src/main/java/hw7/Fibonacci.java
+++ src/main/java/hw7/Fibonacci.java
@@ -11,7 +11,7 @@ public class Fibonacci {
 
         for (int i = 0; i < n; i++) { // цикл будет работаеть пока i<10, каждую итерацию увеличиваясь на 1.
 
-            System.out.print(+a + " ");
+            System.out.print(a + " ");
 
             int nextNumber = a + b;   // след число в ряду
             a = b;                    // двигаем a на одну позицию вперёд
