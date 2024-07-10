805903a6a0e9ab7ddd4df427bf7e1f4ed7e5ed34
TGPvs <tgpavs@gmail.com>
2024-07-10 14:25:33 +0300

    next hw update

--- src/main/java/hw6/Fibonacci.java
+++ src/main/java/hw6/Fibonacci.java
@@ -1,19 +1,20 @@
-package hw7;
+package hw6;
 
 public class Fibonacci {
 
     public static void fibNumber() {
-        int n = 10;                   //сколько всего будем выводить чисел
-        int a = 0;                    //первое число
-        int b = 1;                    //второе число
 
-        System.out.println("Первые " + n + " чисел из ряда Фибоначчи: ");
+        int a = 0;   //первое число
+        int b = 1;   //второе число
+        int n = 10;  //сколько всего будем выводить чисел
+
+        System.out.println("Первые " + n + " чисел из ряда Фибоначчи:");
 
         for (int i = 0; i < n; i++) { // цикл будет работаеть пока i<10, каждую итерацию увеличиваясь на 1.
 
             System.out.print(a + " ");
 
-            int nextNumber = a + b;   // след число в ряду
+            int nextNumber = a + b;   // вычислить след. число в ряду Ф-и
             a = b;                    // двигаем a на одну позицию вперёд
             b = nextNumber;           // двигаем b на одну позицию вперёд
         }
